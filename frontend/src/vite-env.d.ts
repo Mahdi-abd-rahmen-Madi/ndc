584a295eff504932b35cb4777bf06579b3a27cff
Mahdi <mahdiabderrahmen@tutanota.com>
2026-08-21 08:34:21 +0100

    chore: add VITE_WORKER_URL to env type definitions

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="maplibre-gl" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL?: string
+  readonly VITE_API_URL?: string;
+  readonly VITE_WORKER_URL?: string;
 }
 
 interface ImportMeta {
